6b73869f8fe44252b55335143dde1a77140eba53
John D Jones III <jnbek1972@gmail.com>
2012-06-27 15:59:03 -0600

    Move Install Kernel into here.

--- installworld.sh
+++ installworld.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Run after the FreeBSD make world and kernel build/installs are done in makeworld.sh
 # Best if run in Single User Mode.
+KERNCONF='FILESERVER_8'
 cd /usr/src
+make installkernel KERNCONF=$KERNCONF
 make -j4 installworld
 cd /usr/src/usr.sbin/mergemaster
 ./mergemaster.sh -Ui
